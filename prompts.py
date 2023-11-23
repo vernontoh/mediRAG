a187325a154f7417181f27e82fcf2edc99222e34
Tej-Deep <ptejdeep@gmail.com>
2023-11-23 18:06:11 +0800

    including results from testing

--- prompts.py
+++ prompts.py
@@ -48,7 +48,7 @@ Instruction: Use the following pieces of context to answer the question at the e
 {question} 
 
 Give a detailed answer with the help of the provided piece of information. Always end with "The Answer is [Yes,No,Maybe]" to answer the question based on the provided information.
-[/INST]""" ## Acc = 0.3 for 10 samples
+[/INST]""" ## Acc = 0.3 for 10 samples BERTScore: {'precision': 0.8313113331794739} BLEU: 0.031105619821945752
 
 PROMPT_TEMPLATE_QA_ANSWER_START = """### [INST] 
 Instruction: Use the following pieces of context to answer the question at the end.
@@ -59,4 +59,4 @@ Instruction: Use the following pieces of context to answer the question at the e
 {question} 
 
 Give a detailed answer with the help of the provided piece of information. Always start with "The Answer is [Yes,No,Maybe]" to answer the question based on the provided information.
-[/INST]""" ## Acc = 0.6 for 10 samples
+[/INST]""" ## Acc = 0.6 for 10 samples BERTScore: {'precision': 0.821868222951889}, BLEU: 0.024074726951251374
